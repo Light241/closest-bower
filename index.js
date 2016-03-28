7395b3e757fe6215488242222fc60efa7db6c45d
Sergey Panfilov <sp@arex.io>
2016-03-28 13:18:49 +0800

    checkstyle

--- index.js
+++ index.js
@@ -1,76 +1,83 @@
 var path = require('path');
-var fs   = require('fs');
+var fs = require('fs');
 
 module.exports = closest;
 module.exports.sync = closestSync;
+
+var fileName = 'bower.json';
 
 function closest(dirname, filter, found) {
   dirname = path.resolve(dirname);
 
   if (!found) {
-    found  = filter;
-    filter = truthy
+    found = filter;
+    filter = truthy;
   }
 
   check();
   function check() {
     if (isRoot(dirname)) return found(null, null);
 
-    var pkgfile = path.join(dirname, 'bower.json');
+    var pkgfile = path.join(dirname, fileName);
 
-    fs.exists(pkgfile, function(exists) {
+    fs.exists(pkgfile, function (exists) {
       if (!exists) return next();
 
-      read(pkgfile, function(err, pkg) {
+      read(pkgfile, function (err, pkg) {
         if (err) return found(err);
         if (filter(pkg, pkgfile)) return found(null, pkgfile);
-        next()
-      })
-    })
+        next();
+      });
+    });
   }
 
   function next() {
     dirname = path.join(dirname, '..');
-    check()
+    check();
   }
 }
 
 function closestSync(dirname, filter) {
   dirname = path.resolve(dirname);
-  filter  = filter || truthy;
+  filter = filter || truthy;
 
   do {
-    var pkgfile = path.join(dirname, 'bower.json');
+    var pkgfile = path.join(dirname, fileName);
     if (!fs.existsSync(pkgfile)) continue;
     var pkg = readSync(pkgfile);
-    if (filter(pkg, pkgfile)) return pkgfile
+
+    if (filter(pkg, pkgfile)) {
+      return pkgfile;
+    }
   } while (!isRoot(
-    dirname = path.join(dirname, '..')
+      dirname = path.join(dirname, '..')
   ));
 
-  return null
+  return null;
 }
 
 function isRoot(dirname) {
-  return path.resolve(dirname, '..') === dirname
+  return path.resolve(dirname, '..') === dirname;
 }
 
 function read(pkg, done) {
-  fs.readFile(pkg, 'utf8', function(err, json) {
+  fs.readFile(pkg, 'utf8', function (err, json) {
     if (err) return done(err);
 
     try {
-      json = JSON.parse(json)
-    } catch(e) { done(e) }
+      json = JSON.parse(json);
+    } catch (e) {
+      done(e);
+    }
 
-    return done(null, json)
+    return done(null, json);
   })
 }
 
 function readSync(pkg) {
-  return JSON.parse(fs.readFileSync(pkg, 'utf8'))
+  return JSON.parse(fs.readFileSync(pkg, 'utf8'));
 }
 
 function truthy() {
-  return true
+  return true;
 }
